1c038dab685efe784bffac22e66b472dbfb3a5a8
chrisenitan <enitanchris@gmail.com>
2021-07-09 20:24:19 +0200

    add sekctor

---
+++ ios/UITestingTutorialUITests/selectors.swift
@@ -0,0 +1,24 @@
+//
+//  strings.swift
+//  UITestingTutorialUITests
+//
+//  Created by Chris Enitan on 09.07.21.
+//  Copyright © 2021 Code Pro. All rights reserved.
+//
+
+import XCTest
+
+let app = XCUIApplication()
+
+struct selector {
+    static var fName = "Chris"
+    static var password = "yUMMY"
+    static var tablesQuery = app.tables
+    static var radioStaticText = app.tables/*@START_MENU_TOKEN@*/.staticTexts["Radio"]/*[[".cells.staticTexts[\"Radio\"]",".staticTexts[\"Radio\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+    static var justForYouBtn = app.tables/*@START_MENU_TOKEN@*/.staticTexts["Just For You"]/*[[".cells.staticTexts[\"Just For You\"]",".staticTexts[\"Just For You\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+    static var loginButton = app/*@START_MENU_TOKEN@*/.staticTexts["Login"]/*[[".buttons[\"Login\"].staticTexts[\"Login\"]",".staticTexts[\"Login\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+    static var profileButton = app.navigationBars["Mockify Music"].buttons["Profile"]
+}
+
+
+

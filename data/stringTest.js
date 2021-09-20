74124d9395bdc46ddb7e9531881c03b7b7d131bf
chrisenitan <enitanchris@gmail.com>
2021-09-20 08:08:56 +0200

    comments

--- data/stringTest.js
+++ data/stringTest.js
@@ -1,3 +1,4 @@
+//node data/stringTest.js
 //Count occurences of vowels in any given string
 //I opted to use a reduce method so we have a modular app that can be improved to support ...consonants or...
 //Assertions can also be done via chai
@@ -29,7 +30,6 @@ assertVowels({
 
 
 // METHOD 2
-//without using any js method
 function checkAge(reqString) {
   var reqArray = reqString.toUpperCase().split("")
   var vowelTotal = 0

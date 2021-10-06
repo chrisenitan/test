9335d7ce63abf575026e944517bf41af83750ef0
chrisenitan <enitanchris@gmail.com>
2021-10-06 09:23:34 +0200

    async pupp test

---
+++ web/puppeteer/pageTest.js
@@ -0,0 +1,29 @@
+const puppeteer = require("puppeteer")
+
+describe("Basics", () => {
+  beforeEach(async function () {
+    const browser = await puppeteer.launch({
+      headless: false,
+      devtools: true,
+    })
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1400, height: 760 })
+    await page.goto("https://www.google.com")
+    Object.assign(this.currentTest, {
+      browser,
+      page,
+    })
+  })
+  it("Page test", async function () {
+    const browser = this.test.browser
+    const page = this.test.page
+
+    await page.waitForSelector("#L2AGLb")
+    page.click("#L2AGLb", { clickCount: 2, delay: 500 })
+    //page.waitForTimer(2000)
+    /*  await root.page.waitForSelector("input[title='Search']")
+  await root.page.type("input[title='Search']", "Hello")
+  page.click("#hplogo") */
+    await browser.close()
+  })
+})

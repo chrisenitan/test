d4d6405e123c2955614907dc5628e76c9dbff4ce
chrisenitan <enitanchris@gmail.com>
2021-10-07 08:26:16 +0200

    ignore added and new dom tests

--- web/puppeteer/pageTest.js
+++ web/puppeteer/pageTest.js
@@ -19,13 +19,34 @@ describe("Basics", () => {
     const page = this.test.page
 
     await page.waitForSelector("#L2AGLb")
-    page.click("#L2AGLb", { clickCount: 2, delay: 500 })
+    await page.screenshot({
+      type: "png",
+      path: "puppeteer/screenshots/cookies.png",
+      fullpage: true,
+    })
     console.time("test: ")
-    page.waitForTimeout(2000)
+    await page.waitForTimeout(2000)
     console.timeEnd("test: ")
     /*  await root.page.waitForSelector("input[title='Search']")
   await root.page.type("input[title='Search']", "Hello")
   page.click("#hplogo") */
+    await browser.close()
+  })
+
+  it("Reload a screenshot", async function () {
+    const browser = this.test.browser
+    const page = this.test.page
+
+    await page.waitForSelector("#S3BnEe")
+    await page.reload()
+    //console.log(process.cwd())
+    await page.click("#L2AGLb", { clickCount: 2, delay: 500 })
+    await page.type('[name="q"]', "Google", { delay: 500 })
+    await page.screenshot({
+      type: "png",
+      path: "puppeteer/screenshots/index.png",
+      fullpage: true,
+    })
     await browser.close()
   })
 })
